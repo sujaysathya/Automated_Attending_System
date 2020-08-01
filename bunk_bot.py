4152b4686a399fadcc96ddd2b4a06232a634dec9
Sujay Sathya <60563051+sujaysathya@users.noreply.github.com>
2020-08-01 15:59:22 +0530

    Update bunk_bot.py

--- bunk_bot.py
+++ bunk_bot.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
         try:
             if(returnComparison("ss1.png", "ss2.png",1)>=5):
 
-                print("my name is inigo montya")
+                
                 a1,b1=returnMostCommonWord("ss2.png")
                 print(a1)
                 print(b1)
